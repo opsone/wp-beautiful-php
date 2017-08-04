913eb9f416bdc9f3fdc9202bd04c0bf4331b2363
Valentin Soyer <valentin.soyer@opsone.net>
2017-08-04 17:46:17 +0200

    Init project

---
+++ beautiful-maker.php
@@ -0,0 +1,70 @@
+<?php
+
+if ( ! defined( 'WP_CLI' ) ) return;
+
+class WP_Beautiful_Command extends WP_CLI_Command {
+
+    private static $_php_fixer_bin;
+
+    private static $_config;
+
+    private static $_php_fixer_options;
+
+    public function __construct() {
+        self::$_php_fixer_bin     = dirname(__FILE__) . "/vendor/bin/php-cs-fixer";
+        self::$_config            = new StdClass();
+        self::$_php_fixer_options = array(
+            '--verbose',
+            '--show-progress=estimating',
+            '--config=' . dirname(__FILE__) . '/config.php'
+        );
+    }
+
+    public function php( $args, $assoc_args ) {
+        $args = self::sanitize_args( __FUNCTION__, $args, $assoc_args );
+
+        if(!empty(self::$_config->what)) {
+            call_user_func( __CLASS__ . "::php_" . self::$_config->what );
+        }
+    }
+
+    private static function php_core() {
+        $path_to_fix = ABSPATH;
+        shell_exec(self::$_php_fixer_bin . " fix " . $path_to_fix . ' ' . implode(' ', self::$_php_fixer_options));
+    }
+
+    private static function php_themes() {
+        $path_to_fix = ABSPATH . "/wp-content/themes/";
+
+        if( empty(self::$_config->themename)) {
+            shell_exec(self::$_php_fixer_bin . " fix " . $path_to_fix . ' ' . implode(' ', self::$_php_fixer_options));
+        } else {
+            shell_exec(self::$_php_fixer_bin . " fix " . $path_to_fix . '/' . self::$_config->themename . ' ' . implode(' ', self::$_php_fixer_options));
+        }
+    }
+
+    private static function sanitize_args( $command, $args, $assoc_args = null ) {
+        $what_param      = "what";
+        $themename_param = "themename";
+
+        $what = '';
+        if ( isset( $assoc_args[$what_param] ) ) {
+            $what = $assoc_args[$what_param];
+            $themename = $assoc_args[$themename_param];
+
+            if ( method_exists( __CLASS__, "{$command}_{$what}" ) ) {
+                self::$_config->what = $what;
+            } else {
+                WP_Cli::error( "Using unknown '$what' parameter for --what argument." );
+            }
+
+            if( is_dir(ABSPATH . "/wp-content/themes/" . $themename) ) {
+                self::$_config->themename = $themename;
+            } else {
+                WP_Cli::error( "Theme '$themename' was not found in your Wordpress install." );
+            }
+        }
+    }
+}
+
+WP_CLI::add_command( 'beautiful', 'WP_Beautiful_Command' );
---
+++ config.php
@@ -0,0 +1,57 @@
+<?php
+  use \Symfony\Component\Yaml\Yaml;
+
+  class WP_Beautiful_Config {
+
+      private $_finder;
+
+      public function __construct()
+      {
+          $this->_finder = \PhpCsFixer\Finder::create()
+              ->exclude('wp-content/plugins')
+              ->notPath('wp-config.php')
+          ;
+
+          if(file_exists('wp-beautiful.yml')) {
+              $config_env = Yaml::parse(file_get_contents('wp-beautiful.yml'));
+
+              if(!empty($config_env['php-fixer']['excludes'])) {
+                  $excludes = $config_env['php-fixer']['excludes'];
+
+                  if(!empty($excludes['files'])) {
+                      foreach ($excludes['files'] as $filename) {
+                          $this->_finder = $this->_finder->notPath($filename);
+                      }
+                  }
+
+                  if(!empty($excludes['folders'])) {
+                      foreach ($excludes['folders'] as $folder) {
+                          $this->_finder = $this->_finder->exclude($folder);
+                      }
+                  }
+              }
+          }
+      }
+
+      public function exportConfig()
+      {
+          return \PhpCsFixer\Config::create()
+              ->setRules([
+                  '@PSR2'                  => true,
+                  'single_quote'           => true,
+                  'trim_array_spaces'      => true,
+                  'no_useless_else'        => true,
+                  'elseif'                 => true,
+                  'binary_operator_spaces' => array(
+                      'align_double_arrow' => true,
+                      'align_equals'       => true
+                  )
+              ])
+              ->setUsingCache(false)
+              ->setFinder($this->_finder)
+          ;
+      }
+  }
+
+  $config = new WP_Beautiful_Config();
+  return $config->exportConfig();
